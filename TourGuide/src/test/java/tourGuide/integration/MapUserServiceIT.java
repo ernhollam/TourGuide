036c6a786efbc5a891f6bbf65fa2cbb5a46c2e88
e.marinasy <ernholla.marinasy@lomaco.fr>
2023-02-17 09:42:38 +0100

    Fix userService unit tests

--- TourGuide/src/test/java/tourGuide/integration/MapUserServiceIT.java
+++ TourGuide/src/test/java/tourGuide/integration/MapUserServiceIT.java
@@ -1,17 +1,11 @@
 package tourGuide.integration;
 
-import gpsUtil.GpsUtil;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import tourGuide.helper.InternalTestHelper;
 import tourGuide.model.User;
-import tourGuide.service.UserService;
-import tourGuide.service.MapRewardsService;
 import tourGuide.service.MapUserService;
 
 import java.util.List;
@@ -22,42 +16,41 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 @ExtendWith(SpringExtension.class)
-@Import(MapUserService.class)
 public class MapUserServiceIT {
-    @Autowired
-    UserService       userService;
-    @Autowired
-    MapRewardsService mapRewardsService;
-    GpsUtil gpsUtil = new GpsUtil();
-    User user;
-    User user2;
 
-    @BeforeEach
-    public void setUp() {
+    MapUserService userService;
+
+    @Test
+    @DisplayName("getAllUsers() should return all existing users")
+    public void getAllUsers() {
+        // reset internal user number
         InternalTestHelper.setInternalUserNumber(0);
-    }
+        // instanciate a userService
+        userService = new MapUserService();
 
-    @BeforeAll
-    public void initializeUsers() {
-        user  = new User(UUID.randomUUID(), "jon", "000", "jon@tourGuide.com");
-        user2 = new User(UUID.randomUUID(), "jon2", "000", "jon2@tourGuide.com");
-
+        // GIVEN two users added
+        User user  = new User(UUID.randomUUID(), "jon", "000", "jon@tourGuide.com");
+        User user2 = new User(UUID.randomUUID(), "jon2", "000", "jon2@tourGuide.com");
         userService.addUser(user);
         userService.addUser(user2);
-    }
-    @Test
-    public void getAllUsers() {
-
+        //WHEN
         List<User> allUsers = userService.getAllUsers();
-
-        // userService.tracker.stopTracking();
-
+        //THEN getAllUsers() should return the two users added previously
         assertTrue(allUsers.contains(user));
         assertTrue(allUsers.contains(user2));
     }
 
     @Test
+    @DisplayName("addUser should create new user")
     public void addUser() {
+        InternalTestHelper.setInternalUserNumber(0);
+        userService = new MapUserService();
+        User user  = new User(UUID.randomUUID(), "jon", "000", "jon@tourGuide.com");
+        User user2 = new User(UUID.randomUUID(), "jon2", "000", "jon2@tourGuide.com");
+
+        userService.addUser(user);
+        userService.addUser(user2);
+
         User retrievedUser  = userService.getUser(user.getUserName());
         User retrievedUser2 = userService.getUser(user2.getUserName());
 
