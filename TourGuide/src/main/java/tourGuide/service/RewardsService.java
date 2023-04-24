8f8f35bc0cb1e40c67fa47820725f439f8cd8b95
ernhollam <ernholla.marinasy@gmail.com>
2023-04-24 11:53:24 +0200

    Add JavaDoc to RewardsService and TourGuideService interfaces

--- TourGuide/src/main/java/tourGuide/service/RewardsService.java
+++ TourGuide/src/main/java/tourGuide/service/RewardsService.java
@@ -6,11 +6,59 @@ import tourGuide.model.User;
 
 import java.util.concurrent.ExecutionException;
 
+/**
+ * Everything related to reward calculation is defined in this interface.
+ */
 public interface RewardsService {
-    void setProximityBuffer(int proximityBuffer);
-    void setDefaultProximityBuffer();
-    void calculateRewards(User user) throws ExecutionException, InterruptedException;
-    boolean isWithinAttractionProximity(Attraction attraction, Location location);
-    int getRewardPoints(Attraction attraction, User user);
-    double getDistance(Location loc1, Location loc2);
+	/**
+	 * Sets the maximal distance between a location and an attraction.
+	 *
+	 * @param proximityBuffer maximal distance
+	 */
+	void setProximityBuffer(int proximityBuffer);
+
+	/**
+	 * Sets default maximal distance to be used when finding if two locations are close enough each other.
+	 */
+	void setDefaultProximityBuffer();
+
+	/**
+	 * Adds rewards for each attraction a user visited.
+	 *
+	 * @param user user for whom the rewards are to be calculated
+	 *
+	 * @throws ExecutionException
+	 * @throws InterruptedException
+	 */
+	void calculateRewards(User user) throws ExecutionException, InterruptedException;
+
+	/**
+	 * Returns true if a user's location is within proximity buffer with attraction's location.
+	 *
+	 * @param attraction location of attraction
+	 * @param location   user's visited location
+	 *
+	 * @return true if the distance between the user's visited location and the attraction is less than proximity buffer
+	 */
+	boolean isWithinAttractionProximity(Attraction attraction, Location location);
+
+	/**
+	 * Returns the number of points to which a user is entitled for visiting a tourist attraction
+	 *
+	 * @param attraction tourist attraction visited by user
+	 * @param user       user for whom the points are to be calculated
+	 *
+	 * @return number of points to which the user is entitled
+	 */
+	int getRewardPoints(Attraction attraction, User user);
+
+	/**
+	 * Returns distance, in miles, between two locations
+	 *
+	 * @param loc1 first location
+	 * @param loc2 second location
+	 *
+	 * @return distance in miles
+	 */
+	double getDistance(Location loc1, Location loc2);
 }
--- TourGuide/src/main/java/tourGuide/service/TourGuideService.java
+++ TourGuide/src/main/java/tourGuide/service/TourGuideService.java
@@ -13,12 +13,93 @@ import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
+/**
+ * Implements TourGuide's basic features.
+ */
 public interface TourGuideService {
 
-    List<UserReward> getUserRewards(User user);
-    VisitedLocation getUserLocation(User user) throws ExecutionException, InterruptedException;
-    List<Provider> getTripDeals(User user);
-    CompletableFuture<VisitedLocation> trackUserLocation(User user) throws ExecutionException, InterruptedException;
-    List<NearbyAttractionViewModel> getNearByAttractions(User user) throws ExecutionException, InterruptedException;
-    Map<UUID, Location> getAllCurrentLocations() throws ExecutionException, InterruptedException;
+	/**
+	 * Returns the list of rewards a user received for each tourist attraction they visited.
+	 *
+	 * @param user user for which the rewards have to be returned
+	 *
+	 * @return a UserReward object which contains information regarding the visited location, the tourist attraction and
+	 * the corresponding points
+	 *
+	 * @see UserReward
+	 * @see VisitedLocation
+	 * @see gpsUtil.location.Attraction
+	 */
+	List<UserReward> getUserRewards(User user);
+
+	/**
+	 * Tracks a user if location history is empty, or returns their last visited location.
+	 *
+	 * @param user user to be tracked.
+	 *
+	 * @return VisitedLocation object which contains their ID, the location and the time they visited the location
+	 *
+	 * @throws ExecutionException
+	 * @throws InterruptedException
+	 * @see VisitedLocation
+	 * @see Location
+	 */
+	VisitedLocation getUserLocation(User user) throws ExecutionException, InterruptedException;
+
+	/**
+	 * Returns a list of providers which is equivalent to list of trip recommendations for user according to their
+	 * travel preferences.
+	 *
+	 * @param user user to find trip recommendations for
+	 *
+	 * @return a list of providers corresponding to user's preferences
+	 *
+	 * @see Provider
+	 * @see tourGuide.model.UserPreferences
+	 */
+	List<Provider> getTripDeals(User user);
+
+	/**
+	 * Returns the location of the mobile phone or the user's laptop.
+	 *
+	 * The method uses asynchronous CompletableFuture to track a user in a separate thread, then updates the user by
+	 * updating their location history and adding the reward points corresponding to their visited locations in another
+	 * thread when we call the get() method.
+	 *
+	 * @param user user to be located
+	 *
+	 * @return a VisitedLocation encapsulated in a CompletableFuture allowing asynchronous behaviour
+	 *
+	 * @throws ExecutionException
+	 * @throws InterruptedException
+	 * @see CompletableFuture
+	 * @see VisitedLocation
+	 */
+	CompletableFuture<VisitedLocation> trackUserLocation(User user) throws ExecutionException, InterruptedException;
+
+	/**
+	 * Get the closest tourist attractions to the user - no matter how far away they are.
+	 *
+	 * @param user user for which the closest tourist attractions are to be found
+	 *
+	 * @return a list of objects that contain: Name of Tourist attraction, Tourist attractions lat/long, The user's
+	 * location lat/long, The distance in miles between the user's location and each of the attractions. The reward
+	 * points for visiting each Attraction.
+	 *
+	 * @throws ExecutionException
+	 * @throws InterruptedException
+	 * @see NearbyAttractionViewModel
+	 */
+	List<NearbyAttractionViewModel> getNearByAttractions(User user) throws ExecutionException, InterruptedException;
+
+	/**
+	 * Returns locations of all users currently using the app, found by consulting their stored location history. For
+	 * performances reasons, gpsUtil is not used.
+	 *
+	 * @return Map with user and their most recent location
+	 *
+	 * @throws ExecutionException
+	 * @throws InterruptedException
+	 */
+	Map<UUID, Location> getAllCurrentLocations() throws ExecutionException, InterruptedException;
 }
